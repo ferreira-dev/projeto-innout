779658b634ea16ad03c8fd659d83329857755278
ferreira-dev <fabroide@gmail.com>
2021-03-04 21:38:06 -0300

    add regras css

---
+++ src/views/template/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+<span>Desenvolvido com</span>
+<span><i class="iconfont-heart text-danger"></i></span>
+<span>Por COD<span class="text-danger">3</span>R</span>
+</footer>
+
+</body>
+</html>
