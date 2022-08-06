8633d7e0d93f28ace209a727b3b9845ed805b6d2
Fabricio Ferreira <fabroide@gmail.com>
2022-08-06 10:37:41 -0300

    finalizado

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+ini_set('default_charset','UTF-8');
+require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+if($uri === '/' || $uri === '' ||  $uri === '/index.php') {
+    $uri = '/day_records.php';
+}
+
+require_once(CONTROLLER_PATH . "/{$uri}");
---
+++ src/views/template/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <span>Desenvolvido com</span>
+    <span><i class="icofont-heart text-danger mx-1"></i></span>
+    <span>por Ferreira<span class="text-danger">D</span>ev</span>
+</footer>
+<script src="assets/js/app.js"></script>
+</body>
+</html>
